a3c9bf43b10032f10e40d06a35af195f1ce86f19
Lucile-S <63453017+Lucile-S@users.noreply.github.com>
2020-10-13 11:10:51 +0200

    [Main script: pdf Metadata/AAV terms -> json]
    This script import utils.py, pubmed.py, text_mining.py  and csv_to_json.py.
    It retrieves  Metadata and AAV related informations from pdf  present in the publication/ folder.
    Then, the informations are gathered into a csv file.
    Finally, the csv file is converted to json in order to be imported into a database

--- src/pdf_infos.py
+++ src/pdf_infos.py
@@ -1,11 +1,8 @@
 """
-From a directory containing several publications in pdf format, this script creates several csv files:
-- 'Publications_Metadata.csv' with the 'Publi_ID', 'Year', 'Authors', 'Title', 'Journal', 'DOI', 'Keywords','Pages','Abstract','Total_word_count','AAV_count','Frequency','Linked_references'and 'AAV_terms' for every publications.
-- 'IDs_table.csv' with the 'pmid','pmcid','DOI', 'Publi_ID', 'Title','PDF_name' for every pdf files.
-- 'Publication_Informations.csv' with the 'Publi_ID', 'Year', 'Authors', 'Title', 'Journal', 'DOI', 'Keywords','Pages','Abstract', 'Total_word_count','AAV_count','Frequency','Linked_references','AAV_terms','AAV_term','AAV_term_count','Frequency_AAV_term'and  'Linked_AAV_references' for each AAV term found in each publications.
-Also 'Publication_Informations.csv' and 'Publications_Metadata.csv' files are converted to json. 
-Those informations are first retrieved from the pdf itself and alternatively using Pubmed API. 
-If an information could not be retrieved neither from the pdf nor from pubmed, None or a empty list is return.
+From a directory containing several publications in pdf format,
+this script creates a csv file with the 'pmid','pmcid','Year','Authors','Title', 'Journal', 'DOI','keywords', 'Abstract' for every publications.
+Those informations (Metadata) are first retrieved from the pdf itself and alternatively using Pubmed API. 
+If an information could not be retrieved neither from the pdf nor from pubmed, None or a empty list is returned.
 """
 import os
 import re
@@ -14,6 +11,8 @@ from io import StringIO, BytesIO
 import numpy as np
 import pandas as pd
 from collections import defaultdict
+# progress bar
+from tqdm import tqdm
 
 # Homemade scripts
 from utils import *
@@ -29,6 +28,9 @@ from pdfrw import PdfReader
 import json
 from bs4 import BeautifulSoup
 
+# log file
+import logging
+
 
 def extract_info(pdf_path, seq_id=1): 
     #-------------------#
@@ -36,7 +38,6 @@ def extract_info(pdf_path, seq_id=1):
     #-------------------#
     Publi_ID = "Pub_{:06d}".format(seq_id)
     
-
     #---------------------------------------------------------------------------------------------------------------#
     #  Metadata retrieve from the pdf - Authors, Title, Journal, Year, Abstract, Keywords, Journal, Pages, doi      #    
     #---------------------------------------------------------------------------------------------------------------#
@@ -51,7 +52,7 @@ def extract_info(pdf_path, seq_id=1):
     Title = METADATA_pdf['Title']
     if not Title:
         Title = os.path.basename(pdf_path).split('_')[1].replace('.pdf','')
-    DOI = METADATA_pdf['doi']
+    DOI = METADATA_pdf['DOI']
     # IDs (pmid, pmcid) retrieving from pubmed API query using the publication title
     # get the pmid via pubmed API query using publication title 
     pmid = get_pmid(Title) 
@@ -63,12 +64,13 @@ def extract_info(pdf_path, seq_id=1):
         if not pmid:
             raise Exception
     except Exception:
-        print("Publication not available on Pubmed or Title/DOI failed to be retrieved from the pdf. Please rename the pdf like the following exemple : '2020_Development of AAV Variants with Human Hepatocyte Tropism and Neutralizing Antibody Escape Capacity' ")
-
+        print("Publication not available on Pubmed or Title/DOI failed to be retrieved from the pdf.")
+        logging.info('Publication not available on Pubmed or Title/DOI failed to be retrieved from the pdf.')
     # Store all informations retrieved in METADATA dictionnary 
     if pmid:
         METADATA = get_metadata_pubmed(pmid) 
         print('Publication available on PUBMED !')
+        logging.info('Publication available on PUBMED !')
     else : 
         METADATA = METADATA_pdf  
 
@@ -117,60 +119,77 @@ def extract_info(pdf_path, seq_id=1):
 
         # list of related-publication references specific to a AAV_term 
         try :
-            related_references = find_AAV_term_related_publications(xml_text, AAV_term)
+            related_references = find_AAV_term_related_publications_xml(xml_text, AAV_term)
         except:
-            related_references = find_AAV_term_related_publications2(splitted_text ,AAV_term)
+            related_references = find_AAV_term_related_publications(splitted_text ,AAV_term)
 
         # get the list of all AAV-related publication references  find in the publication to the METADATA dictionnary 
         Linked_references += related_references
         # store into a dictionnary 
-        AAV_term_dict = {'Linked_references': Linked_references, 'AAV_term':AAV_term ,'AAV_term_count': AAV_count, 'Frequency_AAV_term':Frequency, 'Linked_AAV_references': related_references}
+        #AAV_term_dict = {'Linked_references': Linked_references, 'AAV_term':AAV_term ,'AAV_term_count': AAV_count, 'Frequency_AAV_term':Frequency, 'Linked_AAV_references': related_references}
+        AAV_term_dict = {'AAV_term':AAV_term ,'AAV_term_count': AAV_count, 'Frequency_AAV_term':Frequency, 'Linked_AAV_references': related_references}
         # Combine the 2 dictionnaries and add to a list
         AAV_data.append({**METADATA, **AAV_term_dict})
+    METADATA['Linked_references']=Linked_references
 
     # IDs dictionnary:
     IDs = {'DOI': DOI, 'PMCID': pmcid, 'PMID': pmid, 'Publi_ID':Publi_ID,'Title':Title, 'PDF_name': File_name}
 
     return METADATA, AAV_data, IDs
 
-
+#-----------------------------------------------------------------------------------------------------------------#
+#-------------------------------------------- MAIN----------------------------------------------------------------#
 
 if __name__ == "__main__":
 
     #--------------------#
     # Define directories #
     #--------------------#
+
     ## current directory
     dir = os.getcwd()
+
     ## project directory
     project_dir =  f'{dir}/../'
+
     ##  pdf list from './publications' folder
     pdf_dir = os.path.join(dir, '../publications')
     pdf_path_list = glob.glob(pdf_dir+'/*.pdf')
-    # test fonction
-    #pdf_test= "/home/lucile/Extraction_info_pdf/publications/2019_Using a barcoded AAV capsid library to select for clinically relevant gene therapy vectors.pdf"
+
+    ## output directory 
+    save_dir = os.path.join(dir, '../output')
+    ## log file
+    
+    LOG_FILENAME = "pdf_infos.log"
+    log_path =os.path.join(dir, '..','output', 'log', LOG_FILENAME)
+    logging.basicConfig(filename=log_path, level=logging.INFO)
+    logging.getLogger('pdfminer').setLevel(logging.ERROR)
+
 
     #-----------------#
     # Initialization  #
     #-----------------#
 
-    ## Create a dataframe df to store all results or open a existing one to add new data 
-    csv_name = 'Publication_Metadata'
-    csv_name_2 = 'Publication_Informations'
+    # csv output file names
+    csv_name_Metadata = 'Publication_Metadata'
+    csv_name_Infos = 'Publication_Informations'
 
+    ## Create a dataframe df to store all results or open a existing one to add new data 
     try:
-        df = pd.read_csv(project_dir + csv_name + '.csv')
+        df = pd.read_csv(project_dir + csv_name_Metadata + '.csv')
         IDs_table = pd.read_csv(project_dir + 'IDs_table.csv')
-        AAV_df =pd.read_csv(project_dir + csv_name_2 + '.csv')
+        AAV_df =pd.read_csv(project_dir + csv_name_Infos + '.csv')
         if df is not None:
             print('CSV files exits already, only new data will be append to them.')
+            logging.info('CSV files exits already, only new data will be append to them.')
+            
     except :
         df_column_names=['Publi_ID', 'Year', 'Authors', 'Title', 'Journal', 'DOI', 'Keywords','Pages','Abstract','Total_word_count','AAV_count','Frequency','Linked_references','AAV_terms']
         df  = pd.DataFrame(columns = df_column_names)
         ## Create a table of standard identifiers, i.e. PMCID, PMID, DOI,Publi_ID for each publications 
         IDs_table = pd.DataFrame(columns = ['DOI', 'PMCID', 'PMID', 'Publi_ID', 'Title','PDF_name'])
         ## Create a dataframe df to store all results  specific to a AAV term
-        AAV_df_column_names = ['Publi_ID', 'Year', 'Authors', 'Title', 'Journal', 'DOI', 'Keywords','Pages','Abstract', 'Total_word_count','AAV_count','Frequency','Linked_references','AAV_terms','AAV_term','AAV_term_count','Frequency_AAV_term', 'Linked_AAV_references']
+        AAV_df_column_names = ['Publi_ID', 'Year', 'Authors', 'Title', 'Journal', 'DOI', 'Keywords','Pages','Abstract', 'Total_word_count','AAV_count','Frequency','AAV_terms','AAV_term','AAV_term_count','Frequency_AAV_term', 'Linked_AAV_references']
         AAV_df = pd.DataFrame(columns = AAV_df_column_names)
         
 
@@ -189,53 +208,65 @@ if __name__ == "__main__":
     #-----------------------#
 
     # Loop over all pdf files
-    for pdf_path in pdf_path_list:
+    for pdf_path in tqdm(pdf_path_list):
         # File_name
         File_name = os.path.basename(pdf_path)
         print(File_name)
-
+        
         if File_name in IDs_table['PDF_name'].values:
             print(f'Informations already extracted for this publication: {File_name}')
+            logging.info(f'Informations already extracted for this publication: {File_name}')
             print('-----------NEXT PUBLICATION---------------------')
+            logging.info('-----------NEXT PUBLICATION---------------------')
 
         else : 
             print(f'New publication : {File_name}')
+            logging.info(f'New publication : {File_name}')
             METADATA, AAV_data, IDs =  extract_info(pdf_path, seq_id)
+            logging.info('Information extraction : Metadata and AAV-related data')
                 
             # Dataframe filling (add dictonnary as  row to dataframe)
             df = df.append(METADATA, ignore_index=True)
             IDs_table  = IDs_table.append(IDs,ignore_index=True)
             AAV_df = AAV_df.append(AAV_data, ignore_index=True)
-
+            
             seq_id += 1
   
             print('-----------NEXT PUBLICATION---------------------')
+            logging.info('-----------NEXT PUBLICATION---------------------')
+    logging.info('--------------------Saving---------------------------')
 
     #-------------------------------#
     # save dataframes as csv files  #
     #-------------------------------#
-    csv_name = 'Publication_Metadata'
-    csv_name_2 = 'Publication_Informations'
-    save_dir = pdf_dir + '/../'
+    
 
     #df.drop(['PMID','PMCID'], axis=1, errors='ignore') # remove those columns if exist  before saving
-    df.to_csv(save_dir + csv_name + '.csv', index=False)
-    IDs_table.to_csv(save_dir+'IDs_table.csv', index=False)
-    AAV_df.to_csv( save_dir + csv_name_2 + '.csv', index=False)
-    print('csv files saved in :', save_dir + csv_name)
+    df.to_csv(save_dir + '/' + csv_name_Metadata + '.csv', index=False)
+    IDs_table.to_csv(save_dir+ '/' + 'IDs_table.csv', index=False)
+    AAV_df.to_csv( save_dir + '/' + csv_name_Infos + '.csv', index=False)
+    print('csv files saved in {save_dir}')
+    logging.info('csv files saved in {save_dir}')
     
     #-----------------------------------------------#
     #          Convert  to json                     #
     #     function defined in csv_to_json.py        #
     #-----------------------------------------------#
+
+     # convert IDs_table to json
+    json_output_0 = save_dir + '/' + 'IDs_table' + '.json'
+    save_to_json(IDs_table,json_output_0)
+
  
     # convert df to json
-    json_output_1 = save_dir +  csv_name + '.json'
+    json_output_1 = save_dir + '/' +  csv_name_Metadata + '.json'
     save_to_json(df,json_output_1)
 
     # convert AAV_df to json 
-    json_output_2 = save_dir +  csv_name_2 + '.json'
+    json_output_2 = save_dir + '/' +  csv_name_Infos + '.json'
     save_to_json(AAV_df,json_output_2)
+
+    logging.info(f'json files saved in {save_dir}')
         
     # Checking  by printing dataframe first lignes
     print('---ID_tables---')
@@ -247,21 +278,6 @@ if __name__ == "__main__":
 
  
 
-        
-
-
-
-
-
-
-
-
- 
-
-        
-
-
-
-
+      
 
 
